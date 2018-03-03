15412341ab40ca040244bd9c8f095cb38ded719f
Paweł Stolecki <noigell@gmail.com>
2018-03-03 22:25:07 +0100

    zadanie-8-6

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var a = 2;
+var b = 1;
+var value = (a * a)-(2 * a * b)-(b * b);
+
+console.log(value);
+
+if (value == 0)
+{
+	console.log("Wynik jest równy zero");
+}
+else if (value > 0)
+{
+	console.log("Wynik jest większy od zera");
+}
+else 
+{
+	console.log("Wynik jest mniejszy od zera");
+}
