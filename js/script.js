79e1a9293923f03a4764d1d30f6f12f4b26bca59
Paweł Stolecki <noigell@gmail.com>
2018-03-05 21:25:05 +0100

    zadanie-8-6 poprawki

--- js/script.js
+++ js/script.js
@@ -1,18 +1,15 @@
 var a = 2;
 var b = 1;
-var value = (a * a)-(2 * a * b)-(b * b);
+var value = (a * a) - (2 * a * b) - (b * b);
 
 console.log(value);
 
-if (value == 0)
-{
+if (value == 0) {
 	console.log("Wynik jest równy zero");
 }
-else if (value > 0)
-{
+else if (value > 0) {
 	console.log("Wynik jest większy od zera");
 }
-else 
-{
+else {
 	console.log("Wynik jest mniejszy od zera");
 }
